08f771c2105ca55d5745fd7d722fab7879b68d43
Ali Haghani <alihaghani96@gmail.com>
2016-02-27 22:34:50 -0800

    Added ability to save link to Firebase.

--- popup/popup.js
+++ popup/popup.js
@@ -1,32 +1,50 @@
+
 var ref = new Firebase("https://linksoup.firebaseio.com");
 
+var userData;
+
 function login(){
-	    ref.authWithOAuthPopup("facebook", function(error, authData) {
-				  if (error) {
-						    console.log("Login Failed!", error);
-								  } else {
-										    console.log("Authenticated successfully with payload:", authData);
-												  }
-			})
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+        if (error) {
+        console.log("Login Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+            userData = authData;
+        }
+    });
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-  var addPageButton = document.getElementById('addPage');
-  checkPageButton.addEventListener('click', function() {
 
-    chrome.tabs.getSelected(null, function(tab) {
-      var d = document;
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
+// we would probably save a profile when we register new users on our site
+// we could also read the profile to see if it's null
+// here we will just simulate this with an isNewUser boolean
+var isNewUser = true;
+ref.onAuth(function(authData) {
+  if (authData && isNewUser) {
+    // save the user's profile into the database so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
+    ref.child("users").child(authData.uid).set({
+      provider: authData.provider,
+      name: getName(authData)
     });
-  }, false);
-}, false);
+  }
+});
+// find a suitable name based on the meta info given by each provider
+function getName(authData) {
+  switch(authData.provider) {
+     case 'password':
+       return authData.password.email.replace(/@.*/, '');
+     case 'twitter':
+       return authData.twitter.displayName;
+     case 'facebook':
+       return authData.facebook.displayName;
+  }
+}
+
+function submitLink(currentUrl){
+    var userNameSpace = userData.facebook.displayName;
+    var userName = userNameSpace.replace(' ', '');
+    var userRef = ref.child("users/" + userName + "/links");
+    userRef.update({link: currentUrl.href});
+}
