b402f25255305bb3285dc8777c5ef1a260f674a3
Eric Stroczynski <ericstroczynski@gmail.com>
2016-02-27 21:44:33 -0800

    Moved appropriate files into directories, links now are correctly accessed

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var linkList = [];
+
+function addLinkToList () {
+    var addlink = window.location;
+    
+    linkList.push(addlink);
+    var newlink = "<div><a href='#' class='each-link' scr='" + linkList[linkList.length - 1] + "'>" + linkList[linkList.length - 1] + "</a><br><hr></div>";
+    document.getElementById('links').innerHTML += newlink;
+}
