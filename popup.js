4011077f51e6937eb3496ef4d1f0a45d7af19333
Ali Haghani <alihaghani96@gmail.com>
2016-02-27 21:10:07 -0800

    Firebase Facebook authentication.

--- popup.js
+++ popup.js
@@ -1,20 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var addPageButton = document.getElementById('addPage');
-  checkPageButton.addEventListener('click', function() {
+var ref = new Firebase("https://linksoup.firebaseio.com");
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+function login(){
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+})
+};
